c66b218295183841c105d18e717dbf14f307597e
Inna Sysoeva <71695604+InnaSysoeva@users.noreply.github.com>
2024-08-13 18:42:33 +0300

    update ChatCard.js

--- client/src/components/ChatCard.js
+++ client/src/components/ChatCard.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import '../styles/ChatCard.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import { ProfileImage } from './ProfileImage'
